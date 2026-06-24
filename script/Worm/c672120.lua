14e8c87d731fd78198f556525fc7016524a6fadc
Lovinauld <lovinovargas577@gmail.com>
2026-06-24 00:17:05 -0400

    Finalized the scripting of Worm Fusion

--- script/Worm/c672120.lua
+++ script/Worm/c672120.lua
@@ -6,11 +6,19 @@ function s.initial_effect(c)
 	e1:SetHintTiming(0,TIMING_MAIN_END)
 	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)	
-	
-
+	--Set 1 "W Nebula Meteorite", from your hand, Deck or GY.
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCost(Cost.SelfBanish)
+	e2:SetTarget(s.settarget)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)	
 end
 
-s.listed_names={id}
+s.listed_names={id,90075978}
 s.listed_series={SET_WORM}
 
 function s.mfilter(c)
@@ -38,5 +46,21 @@ function s.summonlimit(e,tc,tp,mg,chk)
 		e1:SetTarget(function(e,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsRace(RACE_REPTILE) end)
 		e1:SetReset(RESET_PHASE|PHASE_END)
 		Duel.RegisterEffect(e1,tp)
+	end
+end
+
+function s.setfilter(c)
+	return c:IsCode(90075978) and c:IsSSetable()
+end
+
+function s.settarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,nil) end
+end
+
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SSet(tp,g)
 	end
 end
