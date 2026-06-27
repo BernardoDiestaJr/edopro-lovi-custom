507ce8c24e35b49de53363f8a1686cccb8bacadd
Lovinauld <lovinovargas577@gmail.com>
2026-06-27 03:05:22 -0400

    Add files via upload

--- script/Ursarctic/c672122.lua
+++ script/Ursarctic/c672122.lua
@@ -3,16 +3,16 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--special summon
 	Ursarctic.AddSpSummonQuickEffect(c,id)
-	--Destroy all monsters your opponent controls, then Special Summon 1 "Ursarctic" monster except itself
+	--Banish 1 card from your opponent's hand face-down, then Special Summon 1 "Ursarctic" monster from Deck or GY, except itself.
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,2))
-	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetTarget(s.desptg)
-	e2:SetOperation(s.despop)
+	e2:SetTarget(s.remsptg)
+	e2:SetOperation(s.remspop)
 	c:RegisterEffect(e2)	
 	--If this card is Tributed: You can add this card to your hand, then take 700 damage
 	local e3=Effect.CreateEffect(c)
@@ -33,17 +33,18 @@ function s.spfilter(c,e,tp)
 	return c:IsSetCard(SET_URSARCTIC) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
-function s.desptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_MZONE,nil)
-	if chk==0 then return #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,tp,0)
+function s.remsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_HAND)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
 end
 
-function s.despop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_MZONE,nil)
-	if #g>0 and Duel.Destroy(g,REASON_EFFECT)>0 then
+function s.remspop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_HAND,nil,tp,POS_FACEDOWN)
+	if #g==0 then return end
+	local rg=g:RandomSelect(tp,1)
+	if #rg>0 and Duel.Remove(rg,POS_FACEDOWN,REASON_EFFECT)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil,e,tp)
 		if #sg>0 then
