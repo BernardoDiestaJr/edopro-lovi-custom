bf83cc03e75895728a7124b881efc443c4e63183
Lovinauld <lovinovargas577@gmail.com>
2026-07-08 01:56:08 -0400

    Add files via upload

---
+++ script/Touhou/c81519994.lua
@@ -0,0 +1,149 @@
+--Onmyō the Dichromatic Shingyoku
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	local sme,soe=Spirit.AddProcedure(c,EVENT_SPSUMMON_SUCCESS)
+	--Mandatory return
+	sme:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	sme:SetTarget(s.mrettg)
+	sme:SetOperation(s.retop)
+	--Optional return
+	soe:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	soe:SetTarget(s.orettg)
+	soe:SetOperation(s.retop)
+	--You can Tribute this card from your hand or field then target 1 "Onmyō" Spell in your GY or banishment; apply that card's activation effect, then shuffle it into the Deck.	
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND|LOCATION_MZONE)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetCondition(function(e,tp) return Duel.IsTurnPlayer(1-tp) end)
+	e1:SetCost(Cost.SelfTribute)
+	e1:SetTarget(s.copytg)
+	e1:SetOperation(s.copyop)
+	e1:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
+	c:RegisterEffect(e1)
+	--Banish all monsters your opponent controls, then you can Normal Summon 1 Spirit monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_REMOVE+CATEGORY_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.remnstg)
+	e2:SetOperation(s.remnsop)
+	c:RegisterEffect(e2)	
+end
+
+s.listed_names={id}
+s.listed_series={0x1fa}
+
+function s.thfilter(c)
+	return c:IsSetCard(0x1fa) and not c:IsCode(id) and c:IsAbleToHand()
+end
+
+function s.mrettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil) end
+	Spirit.MandatoryReturnTarget(e,tp,eg,ep,ev,re,r,rp,1)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED)
+end
+
+function s.orettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil) 
+	and Spirit.OptionalReturnTarget(e,tp,eg,ep,ev,re,r,rp,0) end
+	Spirit.OptionalReturnTarget(e,tp,eg,ep,ev,re,r,rp,1)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED)
+end
+
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SendtoHand(c,nil,REASON_EFFECT)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
+
+function s.copyfilter(c)
+	return c:IsSetCard(0x1fa) and c:IsSpell() and c:IsAbleToDeck() and c:CheckActivateEffect(true,true,false)~=nil
+end
+
+function s.copytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE|LOCATION_REMOVED) and chkc:IsControler(tp) and s.copyfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.copyfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.copyfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,tp,0)
+end
+
+function s.copyop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc:IsRelateToEffect(e) then return end
+	local te,ceg,cep,cev,cre,cr,crp=tc:CheckActivateEffect(true,true,false)
+	if not te then return end
+	local tg=te:GetTarget()
+	local op=te:GetOperation()
+	if tg then tg(te,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,1) end
+	Duel.BreakEffect()
+	tc:CreateEffectRelation(te)
+	Duel.BreakEffect()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	for etc in aux.Next(g) do
+		etc:CreateEffectRelation(te)
+	end
+	if op then op(te,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,1) end
+	tc:ReleaseEffectRelation(te)
+	for etc in aux.Next(g) do
+		etc:ReleaseEffectRelation(te)
+	end
+	Duel.BreakEffect()
+	Duel.SendtoDeck(te:GetHandler(),nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+end
+
+function s.remnstg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
+end
+
+function s.sumfilter(c)
+	return c:IsType(TYPE_SPIRIT) and c:IsSummonable(true,nil)
+end
+
+function s.remnsop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,nil)
+	if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)~=0 then
+		local sg1=Duel.GetMatchingGroup(s.sumfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,nil)
+		if #sg1>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			Duel.BreakEffect()
+			Duel.ShuffleHand(tp)
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+			local sg2=sg1:Select(tp,1,1,nil):GetFirst()
+			Duel.Summon(tp,sg2,true,nil)
+		end
+	end
+end
+
+function s.nsop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.nsfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+		if not g:GetFirst():IsLocation(LOCATION_HAND) then return end
+		local sg1=Duel.GetMatchingGroup(s.sumfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,nil)
+		if #sg1>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.BreakEffect()
+			Duel.ShuffleHand(tp)
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+			local sg2=sg1:Select(tp,1,1,nil):GetFirst()
+			Duel.Summon(tp,sg2,true,nil)
+		end
+	end
+end
