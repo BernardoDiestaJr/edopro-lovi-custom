1b533ec3b93e3495f79be60649f0978db422f226
Lovinauld <lovinovargas577@gmail.com>
2026-07-29 12:25:37 -0400

    Add files via upload

--- script/yang_zing/c672132.lua
+++ script/yang_zing/c672132.lua
@@ -29,6 +29,24 @@ function s.initial_effect(c)
 	e2:SetTarget(s.tdtg)
 	e2:SetOperation(s.tdop)
 	c:RegisterEffect(e2)	
+	--Destruction Check
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetOperation(s.plreg)
+	c:RegisterEffect(e3)
+	--Place this card face-up on your field
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,3))
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCountLimit(1,{id,2})
+	e4:SetTarget(s.pltg)
+	e4:SetOperation(s.plop)
+	e4:SetLabelObject(e3)
+	c:RegisterEffect(e4)
 end
 
 s.listed_series={SET_YANG_ZING}
@@ -68,5 +86,30 @@ function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 			Duel.BreakEffect()
 			Duel.Destroy(dg,REASON_EFFECT)
 		end	
+	end
+end
+
+function s.plreg(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if (r&(REASON_DESTROY|REASON_EFFECT)~=(REASON_DESTROY|REASON_EFFECT)) then return end
+	if Duel.IsPhase(PHASE_STANDBY) then
+		e:SetLabel(Duel.GetTurnCount())
+		c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_STANDBY,0,2)
+	else
+		e:SetLabel(0)
+		c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_STANDBY,0,1)
+	end
+end
+
+function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local c=e:GetHandler()
+	c:ResetFlagEffect(id)
+end
+
+function s.plop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
 	end
 end
