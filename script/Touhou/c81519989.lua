b1f7088b6f9b80301cb307ec8d84fa98471b5724
Lovinauld <lovinovargas577@gmail.com>
2026-08-02 19:07:02 -0400

    Update c81519989.lua

--- script/Touhou/c81519989.lua
+++ script/Touhou/c81519989.lua
@@ -16,14 +16,13 @@ function s.initial_effect(c)
 	--Banish 1 "Windrous" card from your hand, Deck or GY, except "Sanae, Windrous Fantasia Priestess"; Special Summon 1 "Windrous Token" (Fairy/Tuner/WIND/Level 1/ATK 0/DEF 0)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,1))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetCountLimit(1,{id,1})
-	e1:SetCost(s.tkcost)
-	e1:SetTarget(s.tktg)
-	e1:SetOperation(s.tkop)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -96,5 +95,24 @@ function s.tkop(e,tp,eg,ep,ev,re,r,rp)
 		local c=e:GetHandler()
 		local token=Duel.CreateToken(tp,81520000)
 		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+function s.thfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsMonster() and c:IsAbleToHand()
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
+		and not Duel.IsExistingMatchingCard(s.thconfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
