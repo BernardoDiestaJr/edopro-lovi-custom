7e07bafc6010ac684cf79f28b94aa94c49aa8957
Lovinauld <lovinovargas577@gmail.com>
2026-08-02 18:51:53 -0400

    Update c81519989.lua

--- script/Touhou/c81519989.lua
+++ script/Touhou/c81519989.lua
@@ -13,10 +13,26 @@ function s.initial_effect(c)
 	e0:SetTarget(s.selfsptg)
 	e0:SetOperation(s.selfspop)
 	c:RegisterEffect(e0)
+	--Banish 1 "Windrous" card from your hand, Deck or GY, except "Sanae, Windrous Fantasia Priestess"; Special Summon 1 "Windrous Token" (Fairy/Tuner/WIND/Level 1/ATK 0/DEF 0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetCost(s.tkcost)
+	e1:SetTarget(s.tktg)
+	e1:SetOperation(s.tkop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)	
+	
 end
 
 s.listed_series={0x2f1,0x1f8}
-s.listed_names={id}
+s.listed_names={id,81520000}
 
 function s.selfspconfilter1(c)
 	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsType(TYPE_SYNCHRO) and c:IsAbleToGraveAsCost()
@@ -48,4 +64,37 @@ function s.selfspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
 	Duel.SendtoGrave(g,REASON_COST)
+end
+
+
+function s.remfilter(c)
+	return c:IsSetCard(0x2f1) and not c:IsCode(id)
+end
+
+function s.tkcostfilter(c)
+	return s.remfilter(c) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
+end
+
+function s.tkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tkcostfilter,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.tkcostfilter,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+
+function s.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and Duel.IsPlayerCanSpecialSummonMonster(tp,81520000,0x2f1,TYPES_TOKEN+TYPE_TUNER,0,0,1,RACE_FAIRY,ATTRIBUTE_WIND)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+
+function s.tkop(e,tp,eg,ep,ev,re,r,rp)
+	if s.tktg(e,tp,eg,ep,ev,re,r,rp,0) then
+		local c=e:GetHandler()
+		local token=Duel.CreateToken(tp,81520000)
+		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
