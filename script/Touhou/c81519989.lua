f9730819a062ee9b5d014ec606e0cc54fc90fc55
Lovinauld <lovinovargas577@gmail.com>
2026-08-02 18:27:47 -0400

    Add files via upload

--- script/Touhou/c81519989.lua
+++ script/Touhou/c81519989.lua
@@ -1,8 +1,46 @@
 --Sanae the Green Fantasia Priestess
 local s,id=GetID()
 function s.initial_effect(c)
-
+	--Special Summon this card from the hand
+	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,0))
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e0:SetRange(LOCATION_HAND)
+	e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e0:SetCondition(s.selfspcon)
+	e0:SetTarget(s.selfsptg)
+	e0:SetOperation(s.selfspop)
+	c:RegisterEffect(e0)
 end
 
-s.listed_series={}
+s.listed_series={0x2f1,0x1f8}
 s.listed_names={id}
+
+function s.selfspconfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsType(TYPE_SYNCHRO) and c:IsAbleToGraveAsCost()
+end
+
+function s.selfspcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	local g=Duel.GetMatchingGroup(s.selfspconfilter,tp,LOCATION_EXTRA,0,nil)
+	return #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local rg=Duel.GetMatchingGroup(s.selfspconfilter,tp,LOCATION_EXTRA,0,nil)
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_TOGRAVE,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.SendtoGrave(g,REASON_COST)
+end
