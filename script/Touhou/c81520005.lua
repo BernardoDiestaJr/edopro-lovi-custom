29a5d2d867f511a1bbe15da9ac4732bd822c4f44
Lovinauld <lovinovargas577@gmail.com>
2026-08-09 05:28:49 -0400

    Update c81520005.lua

--- script/Touhou/c81520005.lua
+++ script/Touhou/c81520005.lua
@@ -1,4 +1,4 @@
---Windrous Divatar Moriya no Okami
+--Windrous Divitant Moriya no Okami
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
@@ -13,34 +13,26 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCountLimit(1,{id,0})
-	e1:SetTarget(s.sppltg)
-	e1:SetOperation(s.spplop)
-	c:RegisterEffect(e1)
-	--Special Summon this card
+	e1:SetTarget(s.pltg)
+	e1:SetOperation(s.plop)
+	c:RegisterEffect(e1)	
+	--Return up to 3 cards and Special Summon this card in Defense Position
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,2))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_TOEXTRA+CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1,{id,1})
 	e2:SetCondition(function(e) return e:GetHandler():IsContinuousSpell() end)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetTarget(s.rthtg)
+	e2:SetOperation(s.rthop)
 	c:RegisterEffect(e2)
-	--You can return this card to the Extra Deck, then target up to 3 of your "Windrous" cards in your GY or banishment; place them on the bottom of the Deck, then you can Special Summon 1 WIND Tuner from your Deck
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,4))
-	e3:SetCategory(CATEGORY_DESTROY)
+	local e3=e2:Clone()
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1,{id,2})
 	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER|TIMING_MAIN_END)
-	e3:SetCondition(function(e) return e:GetHandler():IsContinuousTrap() end)
-	e3:SetCost(Cost.SelfToExtra)
-	e3:SetTarget(s.tdsptg)
-	e3:SetOperation(s.tdspop)
-	c:RegisterEffect(e3)	
+	e3:SetCondition(function(e) return e:GetHandler():IsContinuousTrap() end)	
+	c:RegisterEffect(e3)
 	
 end
 
@@ -62,12 +54,12 @@ function s.spfilter(c,e,tp,mc)
 	end
 end
 
-function s.sppltg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA|LOCATION_GRAVE)
 end
 
-function s.spplop(e,tp,eg,ep,ev,re,r,rp)
+function s.plop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_EXTRA|LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0
@@ -90,66 +82,24 @@ function s.spplop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rthtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,1-tp,LOCATION_ONFIELD)
+	if chk==0 then return c:IsAbleToExtra() and Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOEXTRA,c,1,tp,LOCATION_STZONE)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,1-tp,LOCATION_ONFIELD)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 
-function s.setfilter(c)
-	return c:IsSetCard(0x2f1) and c:IsSpellTrap() and c:IsSSetable()
-end
-
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0
-		and Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil)
-		and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,3,nil)
-		if #g>0 then
-		Duel.HintSelection(g)
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		end
-	end
-end
-
-function s.cfilter(c)
-	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsMonster() and c:IsFaceup()
-end
-
-function s.tdsptgfilter(c)
-	return c:IsSetCard(0x2f1) and c:IsAbleToDeck() and c:IsFaceup()
-end
-
-function s.tdspfilter(c,e,tp)
-	return c:IsType(TYPE_TUNER) and c:IsAttribute(ATTRIBUTE_WIND) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-
-function s.tdsptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_REMOVED|LOCATION_GRAVE) and chkc:IsControler(tp) and s.tdsptgfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.tdsptgfilter,tp,LOCATION_REMOVED|LOCATION_GRAVE,0,1,nil)
-	and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,0,1,nil)	end
+function s.rthop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,s.tdsptgfilter,tp,LOCATION_REMOVED|LOCATION_GRAVE,0,1,3,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
-end
-
-function s.tdspop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetTargetCards(e)
-	if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_EFFECT)>0 then
-		Duel.ShuffleHand(tp)		
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.tdspfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
-		and Duel.SelectYesNo(tp,aux.Stringid(id,5)) then		
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-		local sg=Duel.SelectMatchingCard(tp,s.tdspfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-			if #sg>0 then
-				Duel.BreakEffect()
-				Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-			end			
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SendtoDeck(c,nil,SEQ_DECKBOTTOM,REASON_EFFECT)>0 and c:IsLocation(LOCATION_EXTRA) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)	
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,1,nil)
+		if #g>0 and Duel.HintSelection(g) and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE) 
+		and Duel.SelectYesNo(tp,aux.Stringid(id,4)) then 	
+			Duel.BreakEffect()
+			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)		
 		end
 	end
 end
