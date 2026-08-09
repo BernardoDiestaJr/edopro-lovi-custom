4432e44349ae84a048027271f493fbe8718a26f1
Lovinauld <lovinovargas577@gmail.com>
2026-08-09 06:41:25 -0400

    Update c81520007.lua

--- script/Touhou/c81520007.lua
+++ script/Touhou/c81520007.lua
@@ -25,18 +25,6 @@ function s.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)	
-	--Add this card from the GY to your hand, and if you do, Set 1 Normal Trap from your Deck with an effect that Special Summons itself as a monster
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SET)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_PHASE+PHASE_END)
-	e3:SetRange(LOCATION_GRAVE)
-	e3:SetCountLimit(1,{id,2})
-	e3:SetCondition(s.tthcon)
-	e3:SetTarget(s.tthtg)
-	e3:SetOperation(s.tthop)
-	c:RegisterEffect(e3)	
 end
 
 s.listed_series={0x2f1,0x2f2}
@@ -51,7 +39,7 @@ function s.selfspcon(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.selfspfilter(c,e,tp)
-	return not c:IsType(TYPE_TUNER) and c:IsSetCard(0x2f1) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK)
+	return not c:IsType(TYPE_TUNER) and c:IsLevel(4) and c:IsRace(RACE_SPELLCASTER) and c:IsDefenseBelow(1000) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK)
 end
 
 function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -103,31 +91,5 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 then
 		Duel.SendtoHand(g,p,REASON_EFFECT)
 		Duel.ConfirmCards(1-p,g)
-	end
-end
-
-function s.tthcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return c:GetTurnID()==Duel.GetTurnCount() and not c:IsReason(REASON_RETURN)
-end
-
-function s.setfilter(c,haszone)
-	return c:IsNormalTrap() and c:IsSSetable(haszone) and c:IsTrapMonster()
-end
-
-function s.tthtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:IsAbleToHand() end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,c,1,tp,0)
-end
-
-function s.tthop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.SendtoHand(c,nil,REASON_EFFECT)>0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-		local sc=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil,false):GetFirst()
-		if sc then 
-			Duel.SSet(tp,sc)
-		end		
 	end
 end
