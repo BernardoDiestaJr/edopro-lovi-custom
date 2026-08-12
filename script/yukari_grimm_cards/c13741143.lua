c22a708247f94dfe509e1ec7957a2ca980df9553
Lovinauld <lovinovargas577@gmail.com>
2026-08-12 07:28:22 -0400

    Update c13741143.lua

--- script/yukari_grimm_cards/c13741143.lua
+++ script/yukari_grimm_cards/c13741143.lua
@@ -7,12 +7,25 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,id)
+	e1:SetCountLimit(1,{id,0})
 	e1:SetCondition(function(_,tp) return Duel.GetFieldGroupCount(tp,LOCATION_EMZONE,0)==0 end)
 	e1:SetCost(s.spcost)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
+	--Grant effect to a "Vicious Theatre" Xyz monster using this card
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(0,TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCost(Cost.DetachFromSelf(1))
+	e2:SetCondition(s.xmatcon)
+	e2:SetOperation(s.xmatop)
+	c:RegisterEffect(e2)	
+	
 end
 
 s.listed_series={0x41e,0x41f,0x420}
@@ -41,4 +54,27 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
+end
+
+function s.xmatcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSetCard(0x41e)
+end
+
+function s.xmatop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		--Unaffected by monster effects
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3101)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_IMMUNE_EFFECT)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetValue(s.efilter)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
+
+function s.efilter(e,re)
+	return re:IsMonsterEffect()
 end
