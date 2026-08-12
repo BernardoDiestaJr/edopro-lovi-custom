45794da6d40961c99fcab53ef1f1e6bf612ee626
Lovinauld <lovinovargas577@gmail.com>
2026-08-12 19:35:50 -0400

    Add files via upload

---
+++ script/yukari_grimm_cards/c13741148.lua
@@ -0,0 +1,106 @@
+--Ɐ.L.I.CE.-I.N.-M.A.R.R.I.A.G.E.
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon 2 "Vicious Gear Tokens" (Machine/DARK/Level 3/ATK 0/DEF 0) in Defense Position, then if you control a "Vicious Theatre" Xyz Monster, you can Special Summon 1 Level 3 "Vicious Theatre" monster from your face-up Extra Deck or GY.
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetCost(s.spcost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--You can banish this card from your GY; add 1 face-up Pendulum Monster from your Extra Deck to your hand, then if your opponent has activated a card effect this turn, you can draw 1 card.
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.thcon)
+	e2:SetCost(Cost.SelfBanish)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)		
+	Duel.AddCustomActivityCounter(id,ACTIVITY_CHAIN,function(re,tp,cid) return rp==1-tp end)
+end
+
+s.listed_series={0x41e}
+s.listed_names={id,13741135,13741144}
+
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST|REASON_DISCARD)
+end
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>=2
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,13741144,0,TYPES_TOKEN,0,0,3,RACE_MACHINE,ATTRIBUTE_DARK) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA|LOCATION_GRAVE)
+end
+
+function s.xyzfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x41e) and c:IsType(TYPE_XYZ)
+end
+
+function s.spfilter(c,e,tp)
+	if not (c:IsSetCard(0x41e) and c:IsLevel(3) and c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)) then return false end
+	if c:IsLocation(LOCATION_GRAVE) then
+		return Duel.GetMZoneCount(tp)>0
+	elseif c:IsLocation(LOCATION_EXTRA) then
+		return Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+	end
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	--Special Summon 2 "Vicious Gear Tokens" (Machine/DARK/Level 3/ATK 0/DEF 0)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<2
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,13741144,0,TYPES_TOKEN,0,0,3,RACE_MACHINE,ATTRIBUTE_DARK) then return end
+	for i=1,2 do
+		local token=Duel.CreateToken(tp,13741144)
+		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+	Duel.SpecialSummonComplete()
+	local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_EXTRA|LOCATION_GRAVE,0,nil,e,tp)
+	if #sg==0 or not Duel.SelectYesNo(tp,aux.Stringid(id,1)) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local ssg=sg:Select(tp,1,1,nil)
+	if #ssg>0 then
+		Duel.BreakEffect()
+		Duel.SpecialSummon(ssg,0,tp,tp,true,false,POS_FACEUP)
+	end	
+end
+
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,13741135),tp,LOCATION_ONFIELD,0,1,nil)
+end
+
+function s.thfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c:IsAbleToHand()
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_EXTRA)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,g)
+		Duel.ShuffleHand(tp)
+		if Duel.GetCustomActivityCount(id,1-tp,ACTIVITY_CHAIN)>0
+			and Duel.IsPlayerCanDraw(tp,1)
+			and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+			Duel.BreakEffect()
+			Duel.Draw(tp,1,REASON_EFFECT)			
+		end	
+	end
+end
