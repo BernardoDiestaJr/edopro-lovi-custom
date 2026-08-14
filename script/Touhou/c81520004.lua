7b63ee5e0392efe0a934b72d87fd7d20bcbf4f04
Lovinauld <lovinovargas577@gmail.com>
2026-08-14 17:50:27 -0400

    Update c81520004.lua

--- script/Touhou/c81520004.lua
+++ script/Touhou/c81520004.lua
@@ -75,12 +75,12 @@ function s.plop(e,tp,eg,ep,ev,re,r,rp)
 		local c=e:GetHandler()
 		if not c:IsRelateToEffect(e) or c:IsImmuneToEffect(e) then return end
 		if Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true) then
-			--Treated as a Continuous Trap
+			--Treated as a Continuous Spell
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 			e1:SetCode(EFFECT_CHANGE_TYPE)
-			e1:SetValue(TYPE_TRAP|TYPE_CONTINUOUS)
+			e1:SetValue(TYPE_SPELL|TYPE_CONTINUOUS)
 			e1:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TURN_SET)
 			c:RegisterEffect(e1)
 		end
