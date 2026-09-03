e68641572c10bd87b7d57e593dc5561246eed57d
Lovinauld <lovinovargas577@gmail.com>
2026-09-02 20:43:47 -0400

    Update c13741141.lua

--- script/yukari_grimm_cards/c13741141.lua
+++ script/yukari_grimm_cards/c13741141.lua
@@ -4,97 +4,88 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Xyz Summon Procedure: 2+ Level 3 "Vicious Theatre" monsters
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x41e),3,3,nil,nil)
-	--Gains 1000 ATK/DEF for each material attached to it
+	--For each material this card has, monsters you control gain 300 ATK and monsters your opponent controls lose 300 ATK/DEF
 	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e0:SetType(EFFECT_TYPE_FIELD)
 	e0:SetCode(EFFECT_UPDATE_ATTACK)
 	e0:SetRange(LOCATION_MZONE)
-	e0:SetValue(function(e,c) return c:GetOverlayCount()*1000 end)
+	e0:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e0:SetValue(function(e,c) return (c:IsControler(e:GetHandlerPlayer()) and 300 or -300)*e:GetHandler():GetOverlayCount() end)
 	c:RegisterEffect(e0)	
 	local e1=e0:Clone()
 	e1:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e1)
-	--If this card has a "Vicious Theatre" card as material, it cannot be destroyed by card effects
+	--If this card is Xyz Summoned, or if your opponent activates a card effect: You can detach 1 material from this card; roll a six-sided die and apply this effect based on the result
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(function(e) return e:GetHandler():GetOverlayGroup():IsExists(Card.IsSetCard,1,nil,0x41e) end)
-	e2:SetValue(1)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DICE+CATEGORY_SET+CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(function(e) return e:GetHandler():IsXyzSummoned() end)
+	e2:SetCost(Cost.DetachFromSelf(1))
+	e2:SetTarget(s.efftg)
+	e2:SetOperation(s.effop)
 	c:RegisterEffect(e2)
-	--You can make all monsters your opponent controls lose ATK/DEF equal to their own Level/Rank/Link x 200, until the end of this turn.
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCondition(function(e) return e:GetHandler():IsXyzSummoned() end)
-	e3:SetTarget(s.atkdeftg)
-	e3:SetOperation(s.atkdefop)
-	c:RegisterEffect(e3)
-	--You can detach 1 material from this card; it can attack again in a row
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetType(EFFECT_TYPE_QUICK_O)
-	e4:SetCode(EVENT_FREE_CHAIN)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1,{id,0})
-	e4:SetHintTiming(0,TIMING_BATTLE_START|TIMING_BATTLE_END)
-	e4:SetCondition(s.atcon)
-	e4:SetCost(Cost.DetachFromSelf(2))
-	e4:SetOperation(s.atop)
-	c:RegisterEffect(e4)	
-	
+	local e3=e2:Clone()
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return rp==1-tp end)
+	c:RegisterEffect(e3)	
 end
 
 s.listed_series={0x41e}
 s.listed_names={id}
+s.roll_dice=true
 
-function s.atkdeffilter(c)
-	return (c:HasLevel() or c:HasRank() or c:HasLink()) and c:IsFaceup()
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
 end
 
-function s.atkdeftg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.atkdeffilter,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
-end
-
-function s.atkdefop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(s.atkdeffilter,tp,0,LOCATION_MZONE,nil)
-	for tc in g:Iter() do
-		local val=tc:HasLevel() and tc:GetLevel()*-200 or tc:GetRank()*-200 or tc:GetLink()*-200
-		--It loses ATK/DEF equal to its own Level/Rank x 200, until the end of this turn
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(val)
-		e1:SetReset(RESETS_STANDARD_PHASE_END)
-		tc:RegisterEffect(e1)
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_UPDATE_DEFENSE)
-		tc:RegisterEffect(e2)
-	end
-end
-
-function s.atcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return Duel.IsBattlePhase()
-end
-
-function s.atop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		--It can make an additional attack during each Battle Phase this turn
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
-		e1:SetCode(EFFECT_EXTRA_ATTACK)
-		e1:SetValue(1)
-		e1:SetReset(RESETS_STANDARD_PHASE_END)
-		c:RegisterEffect(e1)
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
+	local dice=Duel.TossDice(tp,1)
+	if dice>=1 and dice<=3 then
+		--● 1, 2 or 3: Set 1 "Vicious Theatre" Spell/Trap from your Deck. 
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3))
+		local sc=Duel.SelectMatchingCard(tp,nil,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+		if not sc then return end
+		Duel.HintSelection(sc)
+		local b1=sc:IsAbleToChangeControler()
+		local b2=true
+		local op=Duel.SelectEffect(tp,
+			{b1,aux.Stringid(id,4)},
+			{b2,aux.Stringid(id,5)})
+		if op==1 then
+			Duel.GetControl(sc,tp)
+		elseif op==2 then
+			Duel.Destroy(sc,REASON_EFFECT)
+		end
+	elseif dice==4 or dice==5 then
+		--● 4 or 5: Negate the effects of 1 face-up card on the field until the end of the next turn
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g)
+			local tc=g:GetFirst()
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_PHASE|PHASE_END,2)
+			tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(e:GetHandler())
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			e2:SetValue(RESET_TURN_SET)
+			e2:SetReset(RESET_PHASE|PHASE_END,2)
+			tc:RegisterEffect(e2)
+		end
+	elseif dice==6 then
+		--● 6: Inflict 1500 Damage to your opponent
+		Duel.Damage(1-tp,1500,REASON_EFFECT)
 	end
 end
