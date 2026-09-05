10a1e703fd9dc466be22546316b88964f68794ab
Lovinauld <lovinovargas577@gmail.com>
2026-09-04 20:29:25 -0400

    Add files via upload

---
+++ script/yukari_grimm_cards/c13741154.lua
@@ -0,0 +1,160 @@
+--Black Trial Knight Lindamea
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Xyz Summon Procedure: 2 Level 12 DARK monsters
+	Xyz.AddProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK),12,2,s.xyzfilter,aux.Stringid(id,0),2,s.xyzop)
+	--Special Summon this card
+	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,1))
+	e0:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DISABLE)
+	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e0:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e0:SetCode(EVENT_LEAVE_FIELD)
+	e0:SetRange(LOCATION_GRAVE)
+	e0:SetCountLimit(1,{id,1})
+	e0:SetCondition(s.spcon)
+	e0:SetCost(s.spcost)
+	e0:SetTarget(s.sptg)
+	e0:SetOperation(s.spop)
+	c:RegisterEffect(e0)
+	--Gains these effects while in the Extra Monster Zone
+	--● If this card attacks a Defense Position monster, inflict piercing battle damage to your opponent
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_PIERCE)
+	e1:SetRange(LOCATION_EMZONE)
+	c:RegisterEffect(e1)
+	--● Unaffected by your opponent's activated effects, unless they target this card
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetRange(LOCATION_EMZONE)
+	e2:SetValue(s.immval)
+	c:RegisterEffect(e2)
+	--● Once per turn (Quick Effect): You can detach 1 material from this card, then target 1 card your opponent controls; banish it face-down
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_EMZONE)
+	e3:SetCountLimit(1)
+	e3:SetCost(Cost.DetachFromSelf(1))
+	e3:SetTarget(s.rmvtg)
+	e3:SetOperation(s.rmvop)
+	e3:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
+	c:RegisterEffect(e3)	
+	--Count summons
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+		local ge2=ge1:Clone()
+		ge2:SetCode(EVENT_SUMMON_SUCCESS)
+		Duel.RegisterEffect(ge2,0)
+	end)
+end
+
+s.listed_series={0x421}
+s.listed_names={id,13741143}
+
+function s.xyzfilter(c,tp,xyzc)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	return #g>0
+end
+
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	for tc in aux.Next(eg) do
+		Duel.RegisterFlagEffect(tc:GetSummonPlayer(),id,RESET_PHASE|PHASE_END,0,1)
+		if Duel.HasFlagEffect(tc:GetSummonPlayer(),id,5)then
+			Duel.RegisterFlagEffect(tc:GetSummonPlayer(),id+1,RESET_PHASE|PHASE_END,0,5)
+		end
+	end
+end
+
+function s.xyzop(e,tp,chk)
+	if chk==0 then return Duel.HasFlagEffect(1-tp,id+1) end
+	--Cannot Special Summon for the rest of this turn, except non-Warrior "Black Trial" monsters and "Grimm the Tragic Knight"
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,4))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(e,c) return not ((not c:IsRace(RACE_WARRIOR) and c:IsSetCard(0x421)) or c:IsCode(13741143)) end)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	return true
+end
+
+function s.spcostfilter(c)
+	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_DARK)
+end
+
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.spcostfilter,1,true,aux.ReleaseCheckTarget,nil,tg) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.spcostfilter,1,99,true,aux.ReleaseCheckTarget,nil,tg)
+	e:SetLabel(#g)
+	Duel.Release(g,REASON_COST)
+end
+
+function s.cfilter(c,tp,rp)
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousControler(tp) and c:GetPreviousTypeOnField()&TYPE_XYZ~=0 and c:IsPreviousLocation(LOCATION_MZONE)
+		and c:IsPreviousSetCard(0x421) and (c:IsReason(REASON_BATTLE) or (rp==1-tp and c:IsReason(REASON_EFFECT)))
+end
+
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return not eg:IsContains(e:GetHandler()) and eg:IsExists(s.cfilter,1,nil,tp,rp)
+end
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DISABLE,nil,1,1-tp,LOCATION_MZONE)
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local ng=Duel.GetMatchingGroup(Card.IsNegatableMonster,tp,0,LOCATION_MZONE,nil)
+		if #ng==0 or not Duel.SelectYesNo(tp,aux.Stringid(id,3)) then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+		local sc=ng:Select(tp,1,1,nil):GetFirst()
+		if sc then
+			Duel.HintSelection(sc)
+			sc:NegateEffects(e:GetHandler())
+		end
+	end
+end
+
+function s.immval(e,re)
+	local c=e:GetHandler()
+	if not (re:IsActivated() and c:IsXyzSummoned() and e:GetOwnerPlayer()==1-re:GetOwnerPlayer()) then return false end
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return true end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	return not g or not g:IsContains(c)
+end
+
+function s.rmvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,tp,0)
+end
+
+function s.rmvop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEDOWN,REASON_EFFECT)
+	end
+end
+
